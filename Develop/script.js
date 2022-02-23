a8624d433a22a7346c373188927ef0d92087f935
michael <shinlin09@gmail.com>
2022-02-23 12:14:53 -0500

    password1 properties

--- Develop/script.js
+++ Develop/script.js
@@ -3,13 +3,12 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-    var password = generatePassword();
+    var password1 = generatePassword();
     var passwordText = document.querySelector("#password");
 
     passwordText.value = password;
 
 }
-
 /*add prompts to add criteria (password length, characters types such as lowercase, uppercase, numeric, special characters)*/
 /*
 function generatePassword() {
@@ -41,18 +40,88 @@ var obj = new prompt(console.log(len))
 generateBtn.addEventListener("click", writePassword);
 
 let password1 = {
-    passLen: 50
+    passLen: '',
+    lower: '',
+    upper: '',
+    numeric: '',
+    special: ''
+    
 }
 
 password1.passLen = prompt("How many characters would you like your password to be? (8 - 128 characters)")
 
+/*start of inputs*/
+
 if (password1.passLen <= 7) {
     alert("Invalid password length. Please enter a password length between 8 and 128 characters.")
-    password1.passLen
 }   else if (password1.passLen >= 128) {
     alert("Invalid password length. Please enter a password length between 8 and 128 characters.")
-    password1.passLen
 }   else {
-    console.log(password1.passLen = prompt);
+    alert("Your password will have " + password1.passLen + " characters.")
+    console.log(password1.passLen);
 }
 
+password1.lower = prompt("Would you like lower case characters in your password?")
+
+if (password1.lower === 'yes') {
+    alert("Lower case letters will be included in your password.")
+    console.log("lower=yes");
+    console.log(password1.lower);
+} else {
+    alert("Lower case letters will not be included in your password.")
+    console.log("lower=no");
+    console.log(password1.lower);
+}
+
+password1.upper = prompt("Would you like upper case characters in your password?")
+
+if (password1.upper === 'yes') {
+    alert("Upper case letters will be included in your password.")
+    console.log("upper=yes");
+    console.log(password1.upper);
+
+} else {
+    alert("Upper case letters will not be included in your password.")
+    console.log("upper=no");
+    console.log(password1.upper);
+
+}
+
+password1.numeric = prompt("Would you like numeric characters in your password?")
+
+if (password1.numeric === 'yes') {
+    alert("Numeric characters will be included in your password.")
+    console.log("numeric=yes");
+    console.log(password1.numeric);
+} else {
+    alert("Numeric characters will not be included in your password.")
+    console.log("numeric=no");
+    console.log(password1.numeric);
+
+}
+
+password1.special = prompt("Would you like special characters in your password?")
+
+if (password1.special === 'yes') {
+    alert("Special characters will be included in your password.")
+    console.log("Special=yes");
+    console.log(password1.special);
+
+} else {
+    alert("Special characters will not be included in your password.")
+    console.log("Special=no");
+    console.log(password1.special);
+}
+
+/*end of inputs*/
+
+if (password1.lower == 'yes' || password1.upper == 'yes' ||  password1.numeric == 'yes' ||  password1.special == 'yes') {
+    alert("Your password will be generated.")
+    function readinputs();
+} else {
+    console.log(password1);
+}
+
+readinputs(password1.passLen, password1.lower, password1.upper, password1.numeric, password1.special) {
+    
+}
