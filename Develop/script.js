8c6035e7794c7c4c079a3cc6e5849a8f4577fd9b
michael <shinlin09@gmail.com>
2022-02-26 18:07:50 -0500

    reordered

--- Develop/script.js
+++ Develop/script.js
@@ -58,6 +58,7 @@ let makeid = function(length) {
         result += characters.charAt(Math.floor(Math.random() * 
         charactersLength));
     }
+    return result;
 }
 
 //Prompt/inputs function
@@ -71,7 +72,6 @@ let startPrompt = function(){
         alert("Invalid password length. Please enter a password length between 8 and 128 characters.")
     }   else {
         alert("Your password will have " + password1.passLen + " characters.")
-        console.log(password1.passLen);
     }
     
     
@@ -79,13 +79,10 @@ let startPrompt = function(){
     
     if (password1.lower === 'yes') {
         alert("Lower case letters will be included in your password.")
-        console.log("lower=yes");
-        console.log(password1.lower);
         addLower();
+
     } else {
         alert("Lower case letters will not be included in your password.")
-        console.log("lower=no");
-        console.log(password1.lower);
     }
     
     
@@ -93,15 +90,10 @@ let startPrompt = function(){
     
     if (password1.upper === 'yes') {
         alert("Upper case letters will be included in your password.")
-        console.log("upper=yes");
-        console.log(password1.upper);
         addUpper();
         
     } else {
         alert("Upper case letters will not be included in your password.")
-        console.log("upper=no");
-        console.log(password1.upper);
-    
     }
     
     
@@ -109,15 +101,10 @@ let startPrompt = function(){
     
     if (password1.numeric === 'yes') {
         alert("Numeric characters will be included in your password.")
-        console.log("numeric=yes");
-        console.log(password1.numeric);
         addNum();
     
     } else {
         alert("Numeric characters will not be included in your password.")
-        console.log("numeric=no");
-        console.log(password1.numeric);
-    
     }
     
     
@@ -125,20 +112,16 @@ let startPrompt = function(){
     
     if (password1.special === 'yes') {
         alert("Special characters will be included in your password.")
-        console.log("Special=yes");
-        console.log(password1.special);
         addSpecial();
-    
     } else {
         alert("Special characters will not be included in your password.")
-        console.log("Special=no");
-        console.log(password1.special);
     }
     
     
     if (password1.lower == 'yes' || password1.upper == 'yes' ||  password1.numeric == 'yes' ||  password1.special == 'yes') {
         alert("Your password will be generated.")
     } 
+    console.log(makeid(password1.passLen));
     return makeid(password1.passLen);
 }
 //End of inputs
